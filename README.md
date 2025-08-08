dfbc4bc2b76609f5c3d589c4b080f65c2af1d0e4
Tiamat <Jaycruz718@gmail.com>
2025-08-08 14:40:41 -0400

    Part 4 of DOM lessons

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-# Intro to DOM
+# Intro To the DOM
 
-## Accessubg DOM Elements
-- document.getElementById(): grab an element by ID
-- document.getElementsByName(): grab an element by NAME
-- document.querySelector(): grab first element that matches the CSS selector. ONly 1 element. Can grab by simple selector as well as class ID, so on
-- document.querySelectorAll(): grabs all elements matching selector
+## Accessing DOM Elements
+- document.getElementById(); - grab an element by ID
+- document.getElementsByName(); - grab element(s) by name attribute. It ALWAYS returns a list (array like object)
+- document.querySelector() - grab first element that matches css selector. ONLY 1 element. Can grab by simple selector as well as class, id, so on
+- document.querySelectorAll(); - grabs all elements matching selector
 
 ## Dom Manipulation
-- document.createElement();
+ ### Creating DOM elements
+ - document.createElement();
 
-### Adding ELements to the DOM
-- parentNode.appendChild(nodeToAdd): add element at END specified parent
-- parentNode.prepend(nodeToAdd): add element to the beginning of specified parent
-- parentNode.insertBefore(nodeToAdd, childNodeToInsertBefore): add element to specified position in parent element
+ ### Adding Elements to the DOM
+ - parentNode.appendChild(nodeToAdd) - add element at END specified parent
+ - parentNode.prepend(nodeToAdd) - add element to the beginning specified parent
+ - parentNode.insertBefore(nodeToAdd, childNodeToInsertBefore) - add element to specified position in parent element
 
-### Removing ELements from the DOM
+
+### Removing Elements from the DOM
+- parentNode.removeChild(nodeTORemove);
+
+### Replacing and ELement from the DOM
 - parentNode.replaceChild(nodeToPut, nodeToReplace)
 
+
 ### Changing Node Content
-- nodeName.textContent: allows change of 
-- nodeName.innerHTML: used to retrieve or set content as HTML
+- nodeName.textContent = `content`; - allows change of text in node
+- nodeName.innerHTML = `content`; - allows addition of text AND HTML in node
 
-
+## Changing DOM Elements Style
+- nodeName.style.cssProperty = "cssPropertyValue";
--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,37 @@
 // Get element By ID example
-let title = document.getElementById("titleBanner");
+// let title = document.getElementById("titleBanner");
 // Storing element node inside of a variable
 // console.log(title);
 
-let listItems = document.getElementsByName("listItem");
+// let listItems = document.getElementsByName("listItem");
 // console.log(listItems);
 
 // Grab element by any selector
-let firstQuery = document.querySelector('.subTitle');
+// let firstQuery = document.querySelector('.subTitle');
 // console.log(firstQuery);
 // console.log(firstQuery.parentNode);
 // console.log(firstQuery.childNodes);
 // onsole.log(firstQuery.nextElementSibling);
 
-let listItem = document.getElementsByTagName("li");
+// let listItem = document.getElementsByTagName("li");
 
-for (let el of listItems) {
+// for (let el of listItems) {
     // console.log(el)
-}
+// }
 
 // Query Selector All 
-let pars = document.querySelectorAll('p');
+// let pars = document.querySelectorAll('p');
  
 // console.log(pars);
 
-let list = document.getElementsByTagName('ul');
+// let list = document.getElementsByTagName('ul');
 // console.log(list[0].childNodes[3]);
 
-let newListItem = document.createElement('li');
-let newTitle = document.createElement('h1');
+// let newListItem = document.createElement('li');
+// let newTitle = document.createElement('h1');
 // console.log(newListItem);
 
-list[0].appendChild(newListItem)
+// list[0].appendChild(newListItem)
 
 // let body = document.getElementsByName('Name');
 
@@ -39,22 +39,85 @@ list[0].appendChild(newListItem)
 
 // list[0].removeChild(list[0].firstChild);
 
-let body = document.getElementsByName('body');
+// let body = document.getElementsByName('body');
  
 // console.log(body[0].childNodes[1]);
 
-body[0].removeChild(body[0].childNodes[1])
+// body[0].removeChild(body[0].childNodes[1])
 
-newListItem.style.backgroundColor = 'green';
-newListItem.style.color ='white';
+// newListItem.style.backgroundColor = 'green';
+// newListItem.style.color ='white';
 
-if(body[0].childNodes[1].hasAttribute('name')) {
-    console.log('Attribute Exists');
+// if(body[0].childNodes[1].hasAttribute('name')) {
+    // console.log('Attribute Exists');
 
-} else {
-    console.log('no attribute found');
-}
+// } else {
+    // console.log('no attribute found');
+// }
 
 // body[0].removeChild(body[0].childNodes[1])
 
 // body[0].classList.tuggle('change')
+
+// let container = document.getElementById('mainContainer');
+
+// console.log(container);
+
+// let title = document.createElement('h1').textContent = `Jennifer's Application`
+
+// console.log(title);
+
+// container.appendChild(title);
+
+// function createCard(title, content){
+  // create a fragment
+  /* let frag = document.createDocumentFragment();
+  let div = document.createElement('div')
+
+  let header = document.createElement('h1');
+  header.textContent = title;
+
+  let contentBody = document.createElement('p');
+  contentBody.textContent = content;
+
+  let hr = document.createElement('hr')
+
+  div.classList.add('card')
+
+  div.appendChild(header);
+  div.appendChild(hr)
+  div.appendChild(contentBody);
+  frag.appendChild(div);
+  
+  body.appendChild(frag);
+
+}
+
+createCard('dylans card', 'lorem ipsum');
+createCard('christinas card', 'lorem ipsum');
+createCard('bandits card', 'lorem ipsum');
+createCard('vincets card', 'lorem ipsum');
+createCard('thumbs card', 'lorem ipsum');
+createCard('zuke card', 'lorem ipsum');
+createCard('dylans card', 'lorem ipsum');
+createCard('dylans card', 'lorem ipsum');
+createCard('dylans card', 'lorem ipsum');
+
+createCard('dylans card', 'lorem ipsum');
+createCard('dylans card', 'lorem ipsum');
+createCard('dylans card', 'lorem ipsum');
+createCard('dylans card', 'lorem ipsum');
+createCard('dylans card', 'lorem ipsum'); */
+
+function createCard(title, content){
+    let temp = document.getElementById('temp');
+    let clone = temp.content.cloneNode(true);
+
+    const title = clone.querySelector('h3');
+    const stuff = clone.querySelector('p');
+
+    Headers.textContent = title;
+    stuff.textContent = content;
+
+    Body.appendChild(frag);
+}
